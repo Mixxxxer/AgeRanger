a52df81bd95cfc4e49fa21991020cb4dc5ffca2c
Daniel Nieuwoudt <mixxxxer599@gmail.com>
2017-05-10 12:53:26 +1200

    Added positive and negative tests for AddPerson on the PersonController.

--- AgeRanger.Tests.Unit/Web/Controllers/PersonControllerTests.cs
+++ AgeRanger.Tests.Unit/Web/Controllers/PersonControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Mvc;
 using AgeRanger.Controllers;
+using AgeRanger.Domain.Models;
 using AgeRanger.Domain.Services;
+using AgeRanger.Tests.Unit.Helpers;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -19,7 +21,44 @@ namespace AgeRanger.Tests.Unit.Web.Controllers
 
             var returned = controller.Index();
 
-            ((ViewResult) returned).ViewName.Should().Be("Index");
+            ((ViewResult)returned).ViewName.Should().Be("Index");
+        }
+
+        [TestMethod]
+        [TestCategory("Unit-Controllers")]
+        public void AddPerson_Call_Service_When_ModelState_Valid()
+        {
+            var mockService = new Mock<IPersonService>();
+
+            mockService.Setup(m => m.AddPerson(It.IsAny<ConsolidatedPerson>()))
+                .Returns(true);
+
+            var controller = new PersonController(mockService.Object);
+
+            var returned = controller
+                .AddPerson(ConsolidatedPersonBuilder.GetConsolidatedPerson());
+
+            mockService.Verify(m => m.AddPerson(It.IsAny<ConsolidatedPerson>()), Times.Once);
+
+            ((HttpStatusCodeResult) returned).StatusCode.Should().Be(200);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Unit-Controllers")]
+        public void AddPerson_Returns_Badrequest_When_ModelState_Invalid()
+        {
+            var mockService = new Mock<IPersonService>();
+            var controller = new PersonController(mockService.Object);
+
+            controller.ModelState.AddModelError("a", "b");
+
+            var returned = controller
+                .AddPerson(ConsolidatedPersonBuilder.GetConsolidatedPerson());
+
+            mockService.Verify(m => m.AddPerson(It.IsAny<ConsolidatedPerson>()), Times.Never);
+
+            ((HttpStatusCodeResult)returned).StatusCode.Should().Be(400);
         }
     }
 }
